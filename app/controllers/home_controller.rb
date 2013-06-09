e03484ab290abc7b3ccd19a2f2990fd7a3fdb6bd
kimmanis <kimmanis@gmail.com>
2013-06-08 17:38:14 -0700

    reminder time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,7 @@ class HomeController < ApplicationController
     	@floss_yesterday = Floss.where("user = ? and DATE(created_at) = ?", current_user.id, Date.yesterday).last || 
     	@floss_three = Floss.where("user = ? and DATE(created_at) = ?", current_user.id, Date.today - 2.days).last ||
     	@floss_four = Floss.where("user = ? and DATE(created_at) = ?", current_user.id, Date.today - 3.days).last ||
+    	@floss_five = Floss.where("user = ? and DATE(created_at) = ?", current_user.id, Date.today - 4.days).last ||
     	# decide to show the reminder
     	if (current_user.reminder_time != nil) and (Time.now > current_user.reminder_time) 
     		unless (@floss_today.created_at.today? if @floss_today)  or (Time.now < current_user.next_reminder)
